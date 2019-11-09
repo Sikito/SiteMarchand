74978476253344ccfb77456c63fd5cdd877709fe
Saikou <38886015+Sikito@users.noreply.github.com>
2019-11-09 09:47:17 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SiteMarchand
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img className="logo" src="./mylogo.svg" alt="logo" />
+
+        <nav>
+          <ul className="nav__links">
+            <li>
+              <button>Accueil</button>
+            </li>
+            <li>
+              <button>Produits</button>
+            </li>
+            <li>
+              <button>Espace Client</button>
+            </li>
+            <li>
+              <button>Contact</button>
+            </li>
+            <li>
+              <button>Blog</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
